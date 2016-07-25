18e84c88e920fd3f439746b0e5780d4cdb7d1da7
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2016-07-25 17:41:09 +0200

    Done using express.js

--- server/level 1/index.js
+++ server/level 1/index.js
@@ -8,38 +8,17 @@
  * Copyright 2016 Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
  */
 
-var http = require("http");
-var url = require("url");
+var express = require('express'); 
+var app = express();
 
-function start() {
-  function onRequest(request, response) {
-    var pathname = url.parse(request.url).pathname;
-    var a = 0, b = 0
-    var result = '', operation = ''
-    var aux = []
-    
-    console.log("Request for " + pathname + " received.");
-    var aux = pathname.split("/");
-    aux = aux.slice(1);
-    operation = aux[0]
-    a = parseInt(aux[1])
-    b = parseInt(aux[2])
-    console.log(aux);
-    
-    if (operation === 'sum') 
-      result += a + b
-    else if (operation === 'mul')
-      result += a * b
-    else
-      result += "Not able to process this request URL: " + pathname;
-    
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write(result);
-    response.end();
-  }
-  
-  http.createServer(onRequest).listen(4000);
-  console.log("Server has started.");
-}
+app.get('/sum/:a/:b', function (req, res) {
+  res.send(new String(parseInt(req.params.a)+parseInt(req.params.b)));
+});
 
-start( )
+app.get('/mul/:a/:b', function (req, res) {
+  res.send(new String(parseInt(req.params.a)*parseInt(req.params.b)));
+});
+
+app.listen(4000, function () {
+  console.log('Server listening on port 4000.');
+});
